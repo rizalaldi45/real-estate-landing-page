47f64397eca342799f519304b30504cc6157fb76
rizal aldi <guitartabid@gmail.com>
2023-01-12 23:58:04 +0700

    change featured item image

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -1,34 +1,31 @@
-import BgItem1 from '../images/bg-item1.png'
-import { BedsIcon, SqftIcon, BothIcon } from '../images/svgImages'
+import { BedsIcon, SqftIcon, BothIcon } from "../images/svgImages";
 
-const CardItem = () => {
-    return (
-        <div className="w-[100%] md:w-[30%] h-fit bg-white rounded-2xl mt-6 md:mt-0 mx-auto md:mx-0 shadow-xl">
-            <div 
-                className='w-full h-[50%] bg-cover bg-no-repeat bg-center object-fill' 
-            >
-                <img className='rounded-t-lg' src={BgItem1} alt='' />
-            </div>
-            <div className='p-5 text-left mt-2'>
-                <h1 className='font-bold text-md'>$35000</h1>
-                <h6 className='text-sm text-gray-400'>8502 Preston Rd. Inglewood, Maine 98280</h6>
-                <div className='flex flex-wrap justify-between mt-2'>
-                    <div className='flex items-center'>
-                        <BedsIcon />
-                        <h6 className='text-sm font-semibold pl-3'>5 Beds</h6>
-                    </div>
-                    <div className='flex items-center'>
-                        <SqftIcon />
-                        <h6 className='text-sm font-semibold pl-3'>5 Both</h6>
-                    </div>
-                    <div className='flex items-center mt-3 my-2'>
-                        <BothIcon />
-                        <h6 className='text-sm font-semibold pl-3'>2000sqft</h6>
-                    </div>
-                </div>
-            </div>
+const CardItem = ({ image, price, address, bed, wide, both }) => {
+  return (
+    <div className="w-[100%] md:w-[30%] h-fit bg-white rounded-2xl mt-6 md:mt-0 mx-auto md:mx-0 shadow-xl">
+      <div className="w-full h-[50%] bg-cover bg-no-repeat bg-center object-fill">
+        <img className="rounded-t-lg" src={image} alt="" />
+      </div>
+      <div className="p-5 text-left mt-2">
+        <h1 className="font-bold text-md">${price}</h1>
+        <h6 className="text-sm text-gray-400">{address}</h6>
+        <div className="flex flex-wrap justify-between mt-2">
+          <div className="flex items-center">
+            <BedsIcon />
+            <h6 className="text-sm font-semibold pl-3">{bed} Beds</h6>
+          </div>
+          <div className="flex items-center">
+            <SqftIcon />
+            <h6 className="text-sm font-semibold pl-3">{both} Both</h6>
+          </div>
+          <div className="flex items-center mt-3 my-2">
+            <BothIcon />
+            <h6 className="text-sm font-semibold pl-3">{wide}sqft</h6>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default CardItem
+export default CardItem;
--- src/components/FeaturedItem.jsx
+++ src/components/FeaturedItem.jsx
@@ -1,30 +1,66 @@
-import CardItem from './CardItem'
-import { ArrowIcon } from '../images/svgImages'
+import CardItem from "./CardItem";
+import { ArrowIcon } from "../images/svgImages";
+import BgItem1 from "../images/bg-item1.jpg";
+import BgItem2 from "../images/bg-item2.jpg";
+import BgItem3 from "../images/bg-item3.jpg";
 
 const FeaturedItem = () => {
-    return (
-        <div className="w-full h-auto pb-20">
-            <h1 className="font-bold text-2xl pt-24 text-left">
-                Featured Properties
-            </h1>
-            <div className="xs:flex-col md:flex text-left justify-between pt-3">
-                <div>
-                    <h5>Everything you need to know when you're looking</h5>
-                </div>
-                <div className='flex justify-start md:justify-center items-center mt-2 md:mt-0'>
-                    <h5 className="text-cyan-500 font-bold mr-3">
-                        View All Properties
-                    </h5>
-                    <ArrowIcon />
-                </div>
-            </div>
-            <div className='xs:flex-col md:flex justify-between mt-20'>
-                <CardItem />
-                <CardItem />
-                <CardItem />
-            </div>
-        </div>
-    )
-}
+  const dataListHotels = [
+    {
+      image: BgItem1,
+      price: 3100,
+      address: "8502 Preston Rd. Inglewood, Maine 98280",
+      bed: 3,
+      both: 4,
+      wide: 2300,
+    },
+    {
+      image: BgItem2,
+      price: 2500,
+      address: "1514 Boone Crockett Lane",
+      bed: 2,
+      both: 3,
+      wide: 1700,
+    },
+    {
+      image: BgItem3,
+      price: 3700,
+      address: "2666 Lightning Point Drive",
+      bed: 4,
+      both: 5,
+      wide: 2900,
+    },
+  ];
 
-export default FeaturedItem
+  return (
+    <div className="w-full h-auto pb-20">
+      <h1 className="font-bold text-2xl pt-24 text-left">
+        Featured Properties
+      </h1>
+      <div className="xs:flex-col md:flex text-left justify-between pt-3">
+        <div>
+          <h5>Everything you need to know when you're looking</h5>
+        </div>
+        <div className="flex justify-start md:justify-center items-center mt-2 md:mt-0">
+          <h5 className="text-cyan-500 font-bold mr-3">View All Properties</h5>
+          <ArrowIcon />
+        </div>
+      </div>
+      <div className="xs:flex-col md:flex justify-between mt-20">
+        {dataListHotels.map((data, index) => (
+          <CardItem
+            key={`list-hotel-${index}`}
+            image={data.image}
+            address={data.address}
+            price={data.price}
+            wide={data.wide}
+            both={data.both}
+            bed={data.bed}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedItem;
